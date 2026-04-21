232aaaf85c6a02bfc2686f289dfd29a251190489
Tensae Mekonnen <tmekonnen@smith.edu>
2026-04-21 15:39:14 -0400

    editing more with comments

--- Building.java
+++ Building.java
@@ -1,5 +1,46 @@
 public abstract class Building implements BuildingRequirements {
+
     protected String name;
     protected String address;
     protected int floors;
+
+    /**
+     * Adds an item to the building's inventory
+     */
+    public void addItem(String item) {
+
+    }
+
+    /**
+     * Removes an item from the building's inventory and returns it
+     */
+    public String removeItem(String item) {
+        return null;
+    }
+
+    /**
+     * Describes the interior of the building and lists any items present
+     */
+    public abstract void explore();
+
+    /**
+     * Returns the name of the building
+     */
+    public String getName() {
+        return null;
+    }
+
+    /**
+     * Returns the address of the building
+     */
+    public String getAddress() {
+        return null;
+    }
+
+    /**
+     * Returns the number of floors in the building
+     */
+    public int getFloors() {
+        return 0;
+    }
 }
--- GameMap.java
+++ GameMap.java
@@ -1,7 +1,43 @@
 public class GameMap implements GameMapRequirements {
-    public void explore() {}
-    public void goTo(String destination) {}
-    public void play() {}
-    public void printHelp() {}
-    public boolean handleCommand(String command) { return false; }
+
+    private House house;
+    private AcademicHall academicHall;
+    private DiningHall diningHall;
+    private Building currentBuilding;
+    private Student player;
+
+    /**
+     * Describes the current building the player is standing in
+     */
+    public void explore() {
+
+    }
+
+    /**
+     * Moves the player to a different building on campus
+     */
+    public void goTo(String destination) {
+
+    }
+
+    /**
+     * Starts and runs the main game loop until the player quits
+     */
+    public void play() {
+
+    }
+
+    /**
+     * Prints a list of all available commands to the player
+     */
+    public void printHelp() {
+
+    }
+
+    /**
+     * Reads a player command and carries out the appropriate action
+     */
+    public boolean handleCommand(String command) {
+        return false;
+    }
 }
